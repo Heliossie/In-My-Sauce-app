25c5cc6c2e4e7f3cb18a932a5b9a261e8a142a76
Heliossie <frousseau@gmx.fr>
2021-09-28 16:24:48 +0200

    add fonction and button recipe-info in homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Recipes, RecipeService } from '../recipe.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,7 @@ export class HomePage {
 
   recipes: Recipes[] = [];
 
-  constructor(private recipeService: RecipeService) {
+  constructor(private recipeService: RecipeService, private router: Router) {
     this.recipeService;
   }
   // Méthode d'initialisation éxécuter a chaque ouverture de la page
@@ -18,4 +19,12 @@ export class HomePage {
     this.recipes = this.recipeService.getAll();
   }
 
+  /**
+   * Function permettant de de voir les recettes
+   * @param recipes
+   */
+  showRecipe(recipes: Recipes) {
+    this.recipeService.setCurrent(recipes);
+    this.router.navigateByUrl('recipe-info');
+  }
 }
